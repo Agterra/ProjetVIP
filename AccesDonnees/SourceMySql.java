e2d2acbda491d6fd448a3ff0db6045e0a53d94c2
Agterra <louis.lebrun3@free.fr>
2016-05-04 14:18:05 +0200

    creation source
    source my SQL et création du datasource

--- AccesDonnees/SourceMySql.java
+++ AccesDonnees/SourceMySql.java
@@ -5,10 +5,31 @@
  */
 package AccesDonnees;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.io.FileInputStream;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+import javax.sql.DataSource;
+
 /**
  *
  * @author Louis
  */
 public class SourceMySql {
-    
+     public static DataSource getSource(PasswordAuthentication login) throws Exception {
+        // récupération des informations d'authentification
+        String user = login.getUserName();
+        String pwd = new String(login.getPassword());
+        // création d'un objet Properties à parir du fichier 
+        Properties props = new Properties();
+        FileInputStream fichier = new FileInputStream("src/connexion.properties");
+        props.load(fichier);
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setPortNumber(Integer.parseInt(props.getProperty("port")));
+        ds.setDatabaseName(props.getProperty("service"));
+        ds.setServerName(props.getProperty("serveur"));
+        ds.setUser(user);
+        ds.setPassword(pwd);
+        return ds;
+    }
 }
