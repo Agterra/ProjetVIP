d1650b68a530da9acec2ef5f9732befc3340694b
hormetra <hormetra@free.fr>
2016-05-18 14:55:28 +0200

    combocox

---
+++ Model/ModeleComboBoxCR.java
@@ -0,0 +1,44 @@
+package Model;
+
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.DefaultComboBoxModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Claire
+ */
+public class ModeleComboBoxCR extends DefaultComboBoxModel<String> {
+
+    private List<String> listeItems;
+    private String[] noms = {"Acteur", "Realisateur"};
+    
+
+    public ModeleComboBoxCR() {
+        super();
+        listeItems = Arrays.asList(noms);
+    }
+
+    public void addElement(String uneChaine) {
+        super.addElement(uneChaine);
+    }
+
+    @Override
+    public String getElementAt(int i) {
+        return listeItems.get(i);
+    }
+
+    @Override
+    public int getSize() {
+        return listeItems.size();
+    }
+
+}
+
+
